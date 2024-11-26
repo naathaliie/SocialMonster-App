1fd9fe9365b888214e7a56bf1dff6641e6407864
naathaliie <nathalielarsson@live.se>
2024-11-26 14:34:09 +0100

    test redux

--- Client/SocialMonster-App/app/Screens/ProfilesScreen.tsx
+++ Client/SocialMonster-App/app/Screens/ProfilesScreen.tsx
@@ -1,9 +1,19 @@
+import { RootState } from "@/redux/store";
 import { Text, View } from "react-native";
+import { useSelector } from "react-redux";
 
 export default function ProfileScreen() {
+  //Hämta alla monster från monsterSlicen
+  const monsterProfiles = useSelector((state: RootState) => state.monster);
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Här ska alla monsterprofiler synas</Text>
+      {monsterProfiles.map((monster) => {
+        return (
+          <View>
+            <Text>{monster.name}</Text>
+          </View>
+        );
+      })}
     </View>
   );
 }
