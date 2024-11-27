864636d42880155cde7eef69f195be99be33750d
naathaliie <nathalielarsson@live.se>
2024-11-27 10:32:16 +0100

    fix

--- Client/SocialMonster-App/app/Screens/TheFeedScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function TheFeedScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Här ska alla inlägg synas</Text>
-    </View>
-  );
-}
--- Client/SocialMonster-App/app/_layout.tsx
+++ Client/SocialMonster-App/app/_layout.tsx
@@ -7,11 +7,13 @@ import { useSelector } from "react-redux";
 import UserIcon from "@/components/UserIcon"; // Din UserIcon-komponent
 import HomeScreen from "./Screens/HomeScreen"; // Importera Home-skärmen
 import ProfileScreen from "./Screens/ProfilesScreen"; // Importera Profile-skärmen
+import { useState } from "react";
+import IndexScreen from "./Screens/IndexScreen";
 
 // Skapa en Drawer Navigator
 const Drawer = createDrawerNavigator();
 
-export default function Layout() {
+export default function App() {
   return (
     // Se till att hela applikationen är omsluten av <Provider>
     <Provider store={store}>
@@ -21,58 +23,67 @@ export default function Layout() {
 }
 
 function AppNavigator() {
+  //useState för att visa första sidan när man startar appen
+  const [showWelcomeScreeen, setShowWelcomeScreen] = useState<boolean>(true);
+
   // Hämta currentUser från Redux state
   const currentUser = useSelector((state: RootState) => state.currentuser);
 
   return (
-    <Drawer.Navigator
-      screenOptions={({ navigation }) => ({
-        headerLeft: () => (
-          <TouchableOpacity
-            onPress={() => navigation.toggleDrawer()}
-            style={{ marginLeft: 10 }}
-          >
-            <Ionicons name="menu" size={24} color="black" />
-          </TouchableOpacity>
-        ),
-        headerRight: () => (
-          <TouchableOpacity
-            onPress={() => {
-              console.log("Du klickade på profilikonen");
+    <>
+      {showWelcomeScreeen ? (
+        <IndexScreen setShowWelcomeScreen={setShowWelcomeScreen} />
+      ) : (
+        <Drawer.Navigator
+          screenOptions={({ navigation }) => ({
+            headerLeft: () => (
+              <TouchableOpacity
+                onPress={() => navigation.toggleDrawer()}
+                style={{ marginLeft: 10 }}
+              >
+                <Ionicons name="menu" size={24} color="black" />
+              </TouchableOpacity>
+            ),
+            headerRight: () => (
+              <TouchableOpacity
+                onPress={() => {
+                  console.log("Du klickade på profilikonen");
+                }}
+                style={{ marginRight: 10 }}
+              >
+                {/* Visa användarikon om currentUser finns */}
+                {currentUser && (
+                  <UserIcon monsterImage={currentUser.image} size="small" />
+                )}
+              </TouchableOpacity>
+            ),
+          })}
+        >
+          <Drawer.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              drawerIcon: ({ color, size }) => (
+                <Ionicons name="home" size={size} color={color} />
+              ),
+              drawerActiveTintColor: "pink", // Färg när Drawer-item är aktiv
+              drawerInactiveTintColor: "gray", // Färg när Drawer-item inte är aktiv
+              headerTitle: "", // Döljer titeln från denna Screen
             }}
-            style={{ marginRight: 10 }}
-          >
-            {/* Visa användarikon om currentUser finns */}
-            {currentUser && (
-              <UserIcon monsterImage={currentUser.image} size="small" />
-            )}
-          </TouchableOpacity>
-        ),
-      })}
-    >
-      <Drawer.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          drawerIcon: ({ color, size }) => (
-            <Ionicons name="home" size={size} color={color} />
-          ),
-          drawerActiveTintColor: "pink", // Färg när Drawer-item är aktiv
-          drawerInactiveTintColor: "gray", // Färg när Drawer-item inte är aktiv
-          headerTitle: "", // Döljer titeln från denna Screen
-        }}
-      />
-      <Drawer.Screen
-        name="Användarprofiler"
-        component={ProfileScreen}
-        options={{
-          drawerIcon: ({ color, size }) => (
-            <Ionicons name="person" size={size} color={color} />
-          ),
-          drawerActiveTintColor: "pink", // Färg när Drawer-item är aktiv
-          drawerInactiveTintColor: "gray", // Färg när Drawer-item inte är aktiv
-        }}
-      />
-    </Drawer.Navigator>
+          />
+          <Drawer.Screen
+            name="Användarprofiler"
+            component={ProfileScreen}
+            options={{
+              drawerIcon: ({ color, size }) => (
+                <Ionicons name="person" size={size} color={color} />
+              ),
+              drawerActiveTintColor: "pink", // Färg när Drawer-item är aktiv
+              drawerInactiveTintColor: "gray", // Färg när Drawer-item inte är aktiv
+            }}
+          />
+        </Drawer.Navigator>
+      )}
+    </>
   );
 }
