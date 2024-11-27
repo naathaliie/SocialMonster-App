9a53c1269ea6a5b2e6d0a8569ee56d62f6128d72
naathaliie <nathalielarsson@live.se>
2024-11-27 21:45:52 +0100

    Welcome img

--- Client/SocialMonster-App/app/Screens/IndexScreen.tsx
+++ Client/SocialMonster-App/app/Screens/IndexScreen.tsx
@@ -1,4 +1,12 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
+import {
+  Button,
+  Pressable,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 type IndexScreenProps = {
   setShowWelcomeScreen: (value: boolean) => void; // Funktion som tar en boolean och returnerar void
@@ -9,13 +17,16 @@ export default function IndexScreen({
 }: IndexScreenProps) {
   return (
     <View style={styles.container}>
-      <Text style={styles.textStyle}>
-        Detta får bli första sidan med en knapp som tar dig till "appen"
-      </Text>
-      <Button
-        title="Gå till appen"
-        onPress={() => setShowWelcomeScreen(false)}
-      ></Button>
+      <StatusBar barStyle={"dark-content"} backgroundColor={"#292057"} />
+      <Image
+        source={require("../../assets/images/socialMonsterImg.jpeg")}
+        style={styles.imgStyle}
+        contentFit="cover"
+        allowDownscaling={true}
+      />
+      <Pressable style={styles.btn} onPress={() => setShowWelcomeScreen(false)}>
+        <Text style={styles.btnText}>Gå till appen</Text>
+      </Pressable>
     </View>
   );
 }
@@ -26,12 +37,29 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#25292c",
   },
-  textStyle: {
-    color: "white",
+  imgStyle: {
+    flex: 1, // Täcker hela föräldraview
+    width: "100%",
+    height: "100%",
+    position: "absolute", // Gör att bilden fyller hela skärme
+    backgroundColor: "#0553",
   },
   linkStyle: {
     color: "white",
     textDecorationLine: "underline",
     fontSize: 20,
+  },
+  btn: {
+    backgroundColor: "#08AEAD",
+    borderWidth: 1,
+    borderColor: "#ffffff",
+    borderRadius: 20,
+    padding: 10,
+    marginTop: 500,
+  },
+  btnText: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
